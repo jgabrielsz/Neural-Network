3aec205299e9581b7e15bd9d4545e4cd6b2b4b7c
José Gabriel de Souza Silva <j.gabrielsilva2021@gmail.com>
2022-06-21 15:36:11 -0300

    Update matrix.py
    Methods in matrix.py documented.

--- matrix.py
+++ matrix.py
@@ -20,6 +20,18 @@ class matrix:
 
     @staticmethod
     def array_to_matrix(array) -> 'matrix':
+        """
+        Convert array to matrix
+
+        This static method convert an
+        array to a matrix with one column.
+
+        Parameters:
+        array: array to be converted
+
+        Return:
+        obj Matrix
+        """
         m = matrix(len(array), 1)
         for i in range(len(array)):
             m.matrix[i][0] = array[i]
@@ -27,6 +39,20 @@ class matrix:
 
     @staticmethod
     def map(func, m1, m2) -> 'matrix':
+        """
+        Apply a function on both matrix rows.
+
+        This static method apply a 'map' function on each row of the
+        two matrix coleted.
+
+        Parameters:
+        func: the function
+        m1: matrix 1
+        m2: matrix 2
+
+        Return:
+        obj matrix
+        """
         listmatrix = matrix(m1.rows, m1.cols)
         for row in range(len(m1.matrix)):
             listmatrix.matrix[row] = list(
@@ -35,11 +61,35 @@ class matrix:
 
     @staticmethod
     def add(m1, m2) -> 'matrix':
+        """
+        sum two matrix
+
+        This static method add two matrix
+
+        Parameters:
+        m1: matrix 1 
+        m2: matrix 2
+
+        Return:
+        obj matrix
+        """
         newMatrix = matrix.map(lambda x, y: x + y, m1, m2)
         return newMatrix
 
     @staticmethod
     def multiply(m1, m2) -> 'matrix':
+        """
+        Multiply two matrix
+
+        This static method multiply two matrix
+
+        Parameters:
+        m1: matrix 1 
+        m2: matrix 2
+
+        Return:
+        obj matrix
+        """
         if m1.cols != m2.rows:
             print("Matrices can't be multiplied")
             return
@@ -53,6 +103,17 @@ class matrix:
         return newMatrix
 
     def randomize(self) -> None:
+        """
+        Randomize a matrix
+
+        This method randomize all elements in a matrix
+
+        Parameters: 
+        none
+
+        Return:
+        none
+        """
         for _ in range(self.rows):
             row = list()
             for _ in range(self.cols):
@@ -60,6 +121,18 @@ class matrix:
             self.matrix.append(row)
 
     def sigmoid(self) -> None:
+        """
+        Aplly the sigmoid function
+
+        This method aplly the sigmoid function
+        on all the elemens in a matrix
+
+        Parameters:
+        None
+
+        Return:
+        None
+        """
         def sgm(x): return 1 / (1 + math.exp(-x))
 
         for row in range(len(self.matrix)):
@@ -67,6 +140,8 @@ class matrix:
                 self.matrix[row][col] = sgm(self.matrix[row][col])
 
     def show(self) -> None:
+        """This method make the matrix visualization more friendly"""
         for row in self.matrix:
             print(row)
         print()
+
